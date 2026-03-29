131ff9ad7ab9874caeaf586a3060a3c1be64869a
Wilton23x <ws.salazarb@uea.edu.ec>
2026-03-29 13:05:18 -0500

    eliminar venv del repositorio

--- app.py
+++ app.py
@@ -265,4 +265,5 @@ def usuarios():
 # RUN
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
