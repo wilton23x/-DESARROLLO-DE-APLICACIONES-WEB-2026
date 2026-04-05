58d61b23d9271b647af02b94c620735c0e955735
Wilton23x <ws.salazarb@uea.edu.ec>
2026-04-05 14:23:10 -0500

    Proyecto Biblioteca Virtual Express

--- Proyecto/models/usuario.py
+++
@@ -1,8 +0,0 @@
-from flask_login import UserMixin
-
-class Usuario(UserMixin):
-    def __init__(self, id, nombre, email, password):
-        self.id = id
-        self.nombre = nombre
-        self.email = email
-        self.password = password
